e02deeab6aec1fcf48878e329dd4d77a37c7e0d7
deeptalutmahamud <mddeep22@gmail.com>
2024-05-27 00:44:43 +0600

    Initial Commit

---
+++ Namaz Guide Bangla/Namaz_Guide_BanglaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Namaz_Guide_BanglaApp.swift
+//  Namaz Guide Bangla
+//
+//  Created by Talut mahamud Deep on 27/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Namaz_Guide_BanglaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
